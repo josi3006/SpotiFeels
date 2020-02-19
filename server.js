4a919c02cb8a9306659c54f3bbfc33424727cbad
JGSims <josi3006@hotmail.com>
2020-02-18 20:00:04 -0500

    Testing Travis

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
+var testcommit = 'john';
+
+console.log(testcommit);
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
