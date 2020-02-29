6a646758d4b23250811dcb75e6875ff0b568f2df
JGSims <josi3006@hotmail.com>
2020-02-29 14:10:01 -0500

    Connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,32 @@
 
 // Dependencies
-var Sequelize = require("sequelize");
+// var Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("tamedb", "root", "!@Galaxy", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+// var sequelize = new Sequelize("tamedb", "root", "!@Galaxy", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-if (process.env.jawsdb_url) {
-  var connection = mysql.createConnection(process.env.jawsdb_url)
-} else {
-  var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'tamedb'
-  });
-}
+// if (process.env.jawsdb_url) {
+//   var connection = mysql.createConnection(process.env.jawsdb_url)
+// } else {
+//   var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'password',
+//     database: 'tamedb'
+//   });
+// }
 
 
-connection.connect();
-module.exports = connection;
-// Exports the connection for other files to use
-module.exports = sequelize;
+// connection.connect();
+// module.exports = connection;
+// // Exports the connection for other files to use
+// module.exports = sequelize;
