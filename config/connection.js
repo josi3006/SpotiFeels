930fe50d8efb5915875b8bed4e6974d4b98ff6ed
Zach S <56908620+zsadov1@users.noreply.github.com>
2020-03-03 18:57:27 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("tamedb", "root", "!@Galaxy", {
+var sequelize = new Sequelize("tamedb", "root", "password", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
