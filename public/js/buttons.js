2994e52f0edb259495a73867f087e2198ea4d1ee
JGSims <josi3006@hotmail.com>
2020-03-07 08:36:21 -0500

    Removed commented out code

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -1,27 +1,5 @@
 
 
-// $(window).on("load", () => {
-//     // console.log($(".moodButton"));
-//     $(".moodButton").each(function () {
-//         // console.log(this);            |
-//         // this all needs to be changed  v
-//         $(this).on("click", function (event) {
-//             event.preventDefault();
-//             var mood = $(this).data("mood");
-//             console.log(mood);
-//             var queryString = "SELECT * FROM tameimpala WHERE mood_id = ?  ORDER BY Rand() LIMIT 1";
-
-//             // connection query needs to be defined
-//             connection.query(queryString, [mood], function (err, result) {
-//                 if (err) throw err;
-//                 cb(result);
-//                 console.log(this)
-//             });
-//         });
-
-//     })
-// })
-
 
 $(function () {
     $(".moodBtn").on("click", function (event) {
@@ -39,13 +17,4 @@ $(function () {
     })
 
 });
-
-
-
-
-// function updatePage(result) {
-//     console.log('Response is: ' + result)
-//     res.json();
-// };
-
 
