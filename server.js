d12042d5cce58f1fa420e51db2b0f2cf142d709f
JGSims <josi3006@hotmail.com>
2020-03-27 10:58:11 -0400

    Working on Heroku deployment

--- server.js
+++ server.js
@@ -30,16 +30,15 @@ app.set("view engine", "handlebars");
 
 
 // Requiring our routes
-// require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
-    console.log("------------------------------------------------------------------------");
+    console.log("-----------------------------------------------------------------------------");
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-    console.log("------------------------------------------------------------------------");
+    console.log("-----------------------------------------------------------------------------");
 
   });
 });
